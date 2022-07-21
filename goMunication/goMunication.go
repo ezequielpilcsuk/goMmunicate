840c39b48f505b9f960bb52b1a47d9fc00097530
ezequielpilcsuk-trackstreet <ezequiel.pilcsuk@trackstreet.com>
2022-07-20 23:03:45 -0300

    Fixing structure

--- goMunication/goMunication.go
+++
@@ -1,9 +0,0 @@
-package goMunication
-
-import (
-	"github.com/ezequielpilcsuk/goMunication/group"
-	"github.com/ezequielpilcsuk/goMunication/member"
-)
-
-var myGroup group.Group
-var me member.Member
